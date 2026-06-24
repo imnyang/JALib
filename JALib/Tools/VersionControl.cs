9bc9a450744ebb34cbdf17d642afdf37eef30642
Jongyeol <bcpjy1233@gmail.com>
2026-06-24 14:59:01 +0900

    fix: mod breaking due to changed version field location in r145

--- JALib/Tools/VersionControl.cs
+++ JALib/Tools/VersionControl.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Reflection;
+using UnityEngine;
 
 namespace JALib.Tools;
 
@@ -7,7 +8,8 @@ public static class VersionControl {
     public static Version version;
 
     static VersionControl() {
-        releaseNumber = typeof(GCNS).Field("releaseNumber").GetValue<int>();
+        FieldInfo field = typeof(GCNS).Field("releaseNumber") ?? typeof(ADOBase).Assembly.GetType(nameof(Releases)).Field(nameof(Releases.releaseNumber));
+        releaseNumber = field.GetValue<int>();
 #if !TEST
         Version.TryParse(Application.version, out version);
 #endif
